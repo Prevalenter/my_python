63537a50ba5d5d5f480749505214c09a913be951
434625142 <434625142@qq.com>
2017-12-29 16:45:16 +0800

    Signed-off-by: 434625142 <434625142@qq.com>

--- lesson4/Fibonacci.py
+++ lesson4/Fibonacci.py
@@ -1,12 +1,7 @@
 def Fibona(m):
     '循环方式'
-    now,before=1,1
-#    print(now)
-    lst=[]
-    lst.append(now)
-    for i in range(1,m):
-        lst.append(now)
-        before,now=now,now+before
+    lst=[1,1]
+    for i in range(2,m):lst.append(lst[i-1]+lst[i-2])
     return lst
 
 #递归方式
@@ -16,11 +11,32 @@ def Fibona1(m):
     else: return Fibona1(m-1)+Fibona1(m-2)
 def get_n_Fibona1(n):
     lst=[]
-    for i in range(1,n+1):
-        lst.append(Fibona1(i))
+    for i in range(1,n+1):lst.append(Fibona1(i))
     return lst
 
 #这个循环方式
 print(Fibona(10))
 #这个递归方式
 print(get_n_Fibona1(10))
+'''
+
+#这个循环方式
+
+print(Fibona(10))
+
+#这个递归方式
+
+print(get_n_Fibona1(10))
+#递归方式
+#max为最大循环次数,level为当前递归等级
+lst=[1,1]
+def Fibona_01(level):
+    if level<=2:
+        if(level==1):return lst
+        return 1
+    else:
+        temp=Fibona_01(level-1)+Fibona_01(level-2)
+        if not temp in lst:
+            lst.append(temp)
+        return temp
+'''
