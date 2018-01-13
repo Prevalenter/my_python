e070ff1debae53c81fcc95821d3a84c00ba00ab7
434625142 <434625142@qq.com>
2018-01-14 00:15:23 +0800

    Signed-off-by: 434625142 <434625142@qq.com>

--- lesson8/homework.py
+++ lesson8/homework.py
@@ -10,10 +10,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 #显示直方图
 def show_hist(img1):
-    fig=plt.figure()
+#    fig=plt.figure()
     hist=np.histogram(img1,bins=255)
     hist0,hist1=hist[0],hist[1]
     #很奇怪，这里的hist0与hist1的长度不同，所以进行切片处理
+    plt.subplot(122)
     plt.plot(hist1[0:255],hist0)
     plt.show()
 def on_press(event):
@@ -23,8 +24,18 @@ def on_press(event):
     '''
     x=int(event.xdata)
     y=int(event.ydata)
-    print(img_gray[x-4:x+4,y-5:y+5])
-    
+    table_val=img_gray[x-3:x+4,y-3:y+4]
+    print(table_val)
+    fig3=plt.figure('7*7灰度值显示')
+    plt.subplot(211)
+    col_labels = ['col1','col2','col3','col4','col5','col6','col7']
+    row_labels = ['row1','row2','row3','row4','row5','row6','row7']
+    my_table = plt.table(cellText=table_val, colWidths=[0.12]*7,
+                     rowLabels=row_labels, colLabels=col_labels,
+                     loc='best')
+    plt.show()
+    plt.subplot(212)
+    plt.imshow(table_val,cmap='gray')
 def bench_deal(img1,lst=np.array([[50,100,200,255],[25,75,175,225]])):
     '''
     对图像进行划分区域灰度值归一化
@@ -35,28 +46,31 @@ def bench_deal(img1,lst=np.array([[50,100,200,255],[25,75,175,225]])):
     for i in range(1,len(lst[1])):
         lst_index=np.append(lst_index,np.array([lst[1,i]]*(lst[0,i]-lst[0,i-1])))
     img1=lst_index[img1.astype(np.int32)]
+    
     return img1
        
 img_gray = imread('lena.png',True)
 #显示灰度图
-fig=plt.figure()
+fig1=plt.figure('灰度及其直方图')
+plt.subplot(121)
 plt.imshow(img_gray,cmap='gray')
-fig.canvas.mpl_connect('button_press_event', on_press)
+fig1.canvas.mpl_connect('button_press_event', on_press)
 plt.show()
 show_hist(img_gray)
 
 #阈值梯度处理并显示
 img_bench=bench_deal(img_gray)
-fig=plt.figure()
+fig2=plt.figure('梯度化图像及其直方图')
+plt.subplot(121)
 plt.imshow(img_bench,cmap='gray')
 #打印梯度图鼠标点击处附件七个点
-
 plt.show()
 #显示梯度图的直方统计图
+
 show_hist(img_bench)
-fig=plt.figure()
+
+fig3=plt.figure('假彩色图')
 #显示假彩色图
 plt.imshow(img_gray)
 plt.show()
-
 
